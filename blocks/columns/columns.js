a452e79ab20f0632d22b3c1ee5c6d6dd06c6f6bb
Rémi Faucheux <remifaucheux@MBP-de-Remi-2.lan>
2025-01-11 19:04:09 +0100

    nc

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -5,7 +5,18 @@ export default function decorate(block) {
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
-      col.classList.add('columns');
+      col.classList.add('columns-teaser');
+      col.forEach((column) => {
+        // Create a new div element to wrap the content
+        const wrapperDiv = document.createElement('div');
+        wrapperDiv.classList.add('wrapped-container'); // Optional: add a class to the wrapper div
+
+        // Insert the wrapperDiv before the column element
+        column.parentNode.insertBefore(wrapperDiv, column);
+
+        // Move the column element inside the wrapperDiv
+        wrapperDiv.appendChild(column);
+      });
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
