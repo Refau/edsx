74615e68adbefb9314973549f0736483a573eca0
Rémi Faucheux <remifaucheux@MBP-de-Remi-2.lan>
2025-01-11 17:09:53 +0100

    nc

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -22,5 +22,20 @@ export default async function decorate(block) {
 // Move the second and third child elements into the new wrapper div
   newWrapperDiv.appendChild(secondChild);
   newWrapperDiv.appendChild(thirdChild);
+
+
+  let wrapperDiv = document.querySelector(".new-content-wrapper");
+
+  if (wrapperDiv) {
+    // Select all direct child <div> elements inside .new-content-wrapper
+    let childDivs = wrapperDiv.children;
+
+    // Add classes to each child div
+    if (childDivs.length > 0) childDivs[0].classList.add("content-left");
+    if (childDivs.length > 1) childDivs[1].classList.add("content-right");
+
+    // Add a class to the wrapper to style it
+    wrapperDiv.classList.add("flex-container");
+  }
 }
 
