5503ccecb213c166678a436b6381496078bce4e8
Rémi Faucheux <remifaucheux@MacBook-Pro-de-Remi-2.local>
2025-01-12 22:00:10 +0100

    nc

--- scripts/aem.js
+++ scripts/aem.js
@@ -489,6 +489,20 @@ function decorateSections(main) {
       section.style.backgroundRepeat = "no-repeat";
     }
   });
+  main.querySelectorAll('div[data-image]').forEach( section => {
+    console.log(section);
+    const backgroundData = section.getAttribute('data-image');
+    console.log(backgroundData);
+    // Check if background data exists
+    if (backgroundData) {
+      // Store sections with background data
+      console.log(`Section found with background data: ${backgroundData}`);
+      section.style.backgroundImage = `url('${backgroundData}')`;
+      section.style.backgroundSize = "cover";
+      section.style.backgroundPosition = "center";
+      section.style.backgroundRepeat = "no-repeat";
+    }
+  });
   main.querySelectorAll(':scope > div:not([data-section-status])').forEach((section) => {
     const wrappers = [];
     let defaultContent = false;
